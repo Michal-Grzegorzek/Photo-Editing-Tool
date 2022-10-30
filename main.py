1b629707bf81d83656b1d2daa5f1618f1c7d0987
Mike <michal.grzegorzek33@gmail.com>
2022-10-30 12:55:47 +0100

    Layout change, correction of general errors.

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog, messagebox
-from tkinter.filedialog import askopenfilename, asksaveasfilename
-from PIL import Image, ImageTk, ImageFilter, ImageEnhance, ImageOps, ImageDraw, ImageFont
+from tkinter.filedialog import asksaveasfilename
+from PIL import Image, ImageTk, ImageFilter, ImageOps, ImageDraw, ImageFont
 import os
-import numpy as np
-import matplotlib.font_manager
 
-
-# system_fonts = matplotlib.font_manager.findSystemFonts(fontpaths=None, fontext='ttf')
-# print(system_fonts)
 
 my_w = Tk()
 my_w.geometry("1000x1000")  # Size of the window
@@ -24,9 +19,9 @@ def upload_image():
     global img_path, img
     img_path = filedialog.askopenfilename(initialdir=os.getcwd())
     img = Image.open(img_path)
-    img.thumbnail((350, 350))
+    img.thumbnail((500, 500))
     photo_img = ImageTk.PhotoImage(img)
-    canvas2.create_image(300, 210, image=photo_img)
+    canvas2.create_image(305, 270, image=photo_img)
     canvas2.image = photo_img
 
 
@@ -37,9 +32,9 @@ def reset():
 
     if res:
         img = Image.open(img_path)
-        img.thumbnail((350, 350))
+        img.thumbnail((500, 500))
         photo_img = ImageTk.PhotoImage(img)
-        canvas2.create_image(300, 210, image=photo_img)
+        canvas2.create_image(305, 270, image=photo_img)
         canvas2.image = photo_img
 
 
@@ -54,7 +49,8 @@ def save():
 
 
 def exit():
-    res = messagebox.askyesno(title="Are you sure?", message="Are you sure you want to exit?\n Unsaved changes will be lost.")
+    res = messagebox.askyesno(title="Are you sure?", message="Are you sure you want to exit?\n"
+                                                             "Unsaved changes will be lost.")
 
     if res:
         my_w.destroy()
@@ -64,7 +60,7 @@ def rotate_image(func):
     global img
     img = img.rotate(int(rotate_combo.get()))
     photo_img = ImageTk.PhotoImage(img)
-    canvas2.create_image(300, 210, image=photo_img)
+    canvas2.create_image(305, 270, image=photo_img)
     canvas2.image = photo_img
 
 
@@ -72,7 +68,7 @@ def blur():
     global img
     img = img.filter(ImageFilter.BLUR)
     photo_image = ImageTk.PhotoImage(img)
-    canvas2.create_image(300, 210, image=photo_image)
+    canvas2.create_image(305, 270, image=photo_image)
     canvas2.image = photo_image
 
 
@@ -80,7 +76,7 @@ def emboss():
     global img
     img = img.filter(ImageFilter.EMBOSS)
     photo_image = ImageTk.PhotoImage(img)
-    canvas2.create_image(300, 210, image=photo_image)
+    canvas2.create_image(305, 270, image=photo_image)
     canvas2.image = photo_image
 
 
@@ -88,7 +84,7 @@ def sharpen():
     global img
     img = img.filter(ImageFilter.SHARPEN)
     photo_image = ImageTk.PhotoImage(img)
-    canvas2.create_image(300, 210, image=photo_image)
+    canvas2.create_image(305, 270, image=photo_image)
     canvas2.image = photo_image
 
 
@@ -96,16 +92,15 @@ def find_edges():
     global img
     img = img.filter(ImageFilter.FIND_EDGES)
     photo_image = ImageTk.PhotoImage(img)
-    canvas2.create_image(300, 210, image=photo_image)
+    canvas2.create_image(305, 270, image=photo_image)
     canvas2.image = photo_image
 
 
 def invert_colors():
     global img
-    img= ImageOps.invert(img)
-    # img = ImageOps.posterize(img, 2)
+    img = ImageOps.invert(img)
     photo_image = ImageTk.PhotoImage(img)
-    canvas2.create_image(300, 210, image=photo_image)
+    canvas2.create_image(305, 270, image=photo_image)
     canvas2.image = photo_image
 
 
@@ -114,7 +109,7 @@ def solarize(func):
     for m in range(0, v1.get() + 1):
         img = ImageOps.solarize(img, threshold=m)
         photo_image = ImageTk.PhotoImage(img)
-        canvas2.create_image(300, 210, image=photo_image)
+        canvas2.create_image(305, 270, image=photo_image)
         canvas2.image = photo_image
 
 
@@ -123,7 +118,7 @@ def postersize(func):
     m = v2.get()
     img = ImageOps.posterize(img, m)
     photo_image = ImageTk.PhotoImage(img)
-    canvas2.create_image(300, 210, image=photo_image)
+    canvas2.create_image(305, 270, image=photo_image)
     canvas2.image = photo_image
 
 
@@ -131,7 +126,7 @@ def gray_scale():
     global img
     img = ImageOps.grayscale(img)
     photo_image = ImageTk.PhotoImage(img)
-    canvas2.create_image(300, 210, image=photo_image)
+    canvas2.create_image(305, 270, image=photo_image)
     canvas2.image = photo_image
 
 
@@ -142,7 +137,7 @@ def flip_img(event):
     elif flip_combo.get() == "FLIP TOP TO BOTTOM":
         img = img.transpose(Image.FLIP_TOP_BOTTOM)
     photo_image = ImageTk.PhotoImage(img)
-    canvas2.create_image(300, 210, image=photo_image)
+    canvas2.create_image(305, 270, image=photo_image)
     canvas2.image = photo_image
 
 
@@ -162,7 +157,7 @@ def crop_img():
         right = 0
     img = ImageOps.crop(img, (int(left), int(top), int(right), int(bot)))
     photo_image = ImageTk.PhotoImage(img)
-    canvas2.create_image(300, 210, image=photo_image)
+    canvas2.create_image(305, 270, image=photo_image)
     canvas2.image = photo_image
 
 
@@ -189,7 +184,6 @@ def watermark():
     if font_size == '':
         font_size = 18
 
-
     if color == '':
         color = 'black'
 
@@ -197,23 +191,17 @@ def watermark():
         font_entry = 'arial'
 
     while True:
-        # ("font type",font size)
         font_entry = ImageFont.truetype(f"{font_entry}.ttf", int(font_size))
-
-        # Decide the text location, color and font
-        # (255,255,255)-White color text
         draw.text((int(pos_x), int(pos_y)), text, font=font_entry, fill=color)
-
         photo_image = ImageTk.PhotoImage(img_new)
-        canvas2.create_image(300, 210, image=photo_image)
+        canvas2.create_image(305, 270, image=photo_image)
         canvas2.image = photo_image
 
         res = messagebox.askyesno(title="Watermark save", message="Do you want to save this watermark?")
-        print(res)
-        if res == False:
-            print('end point')
+
+        if not res:
             photo_image = ImageTk.PhotoImage(img)
-            canvas2.create_image(300, 210, image=photo_image)
+            canvas2.create_image(305, 270, image=photo_image)
             canvas2.image = photo_image
         else:
             img = img_new
@@ -225,6 +213,7 @@ def watermark():
 canvas2 = Canvas(my_w, width="600", height="530", relief=RIDGE, bd=2)
 canvas2.place(x=350, y=40)
 
+# the creation of the headings
 title = Label(my_w, text="Photo Editing Tool", font="ariel 17 bold", width=15, anchor='e')
 title.place(x=550, y=10)
 
@@ -246,25 +235,24 @@ pruning_label.place(x=40, y=470)
 watermark_label = Label(my_w, text="Watermark", font="ariel 12 bold", anchor='e', fg='#FD841F')
 watermark_label.place(x=40, y=550)
 
-
-btn_select = Button(my_w, text="Select Image", bg='#4649FF', fg='black',
+# the creation of the four main buttons
+btn_select = Button(my_w, text="Select Image", bg='#4649FF', fg='black', width=10,
               font='ariel 15 bold', relief=GROOVE, command=upload_image)
-btn_select.place(x=400, y=495)
+btn_select.place(x=365, y=620)
 
-btn_reset = Button(my_w, text="Reset", bg='#4649FF', fg='black',
+btn_reset = Button(my_w, text="Reset", bg='#4649FF', fg='black', width=10,
               font='ariel 15 bold', relief=GROOVE, command=reset)
-btn_reset.place(x=550, y=495)
+btn_reset.place(x=515, y=620)
 
-btn_save = Button(my_w, text="Save", bg='#4649FF', fg='black',
+btn_save = Button(my_w, text="Save", bg='#54B435', fg='black', width=10,
               font='ariel 15 bold', relief=GROOVE, command=save)
-btn_save.place(x=700, y=495)
+btn_save.place(x=665, y=620)
 
-btn_exit = Button(my_w, text="Exit", bg='#4649FF', fg='black',
+btn_exit = Button(my_w, text="Exit", bg='#E14D2A', fg='black', width=10,
               font='ariel 15 bold', relief=GROOVE, command=exit)
-btn_exit.place(x=800, y=495)
+btn_exit.place(x=815, y=620)
 
-
-
+# section Edit Colors
 btn_gray = Button(my_w, text="Gray-scale", bg='#3D8361', fg='black', width=10,
                     font='ariel 12 bold', relief=GROOVE, command=gray_scale)
 btn_gray.place(x=40, y=120)
@@ -273,30 +261,44 @@ btn_invert = Button(my_w, text="Invert image", bg='#3D8361', fg='black', width=1
                     font='ariel 12 bold', relief=GROOVE, command=invert_colors)
 btn_invert.place(x=180, y=120)
 
-btn_blur = Button(my_w, text="Blur", bg='#8758FF',fg='black',
+solarize_label = Label(my_w, text="Solarize:", font="ariel 13 bold", width=9, anchor='e')
+solarize_label.place(x=15, y=170)
+v1 = IntVar()
+scale1 = ttk.Scale(my_w, from_=0, to=255, variable=v1,
+                   orient=HORIZONTAL, command=solarize)
+scale1.place(x=150, y=170)
+
+postersize_label = Label(my_w, text="Postersize:", font="ariel 13 bold", width=9, anchor='e')
+postersize_label.place(x=37, y=200)
+v2 = IntVar()
+scale2 = ttk.Scale(my_w, from_=1, to=5, variable=v2,
+                   orient=HORIZONTAL, command=postersize)
+scale2.place(x=150, y=200)
+
+# section Filters
+btn_blur = Button(my_w, text="Blur", bg='#8758FF', fg='black',
                     font='ariel 12 bold', relief=GROOVE, command=blur, width=10)
 btn_blur.place(x=40, y=270)
 
-btn_emboss = Button(my_w, text="Emboss", bg='#8758FF',fg='black',
+btn_emboss = Button(my_w, text="Emboss", bg='#8758FF', fg='black',
                     font='ariel 12 bold', relief=GROOVE, command=emboss, width=10)
 btn_emboss.place(x=180, y=270)
 
-btn_sharpen = Button(my_w, text="Sharpen", bg='#8758FF',fg='black',
+btn_sharpen = Button(my_w, text="Sharpen", bg='#8758FF', fg='black',
                     font='ariel 12 bold', relief=GROOVE, command=sharpen, width=10)
 btn_sharpen.place(x=40, y=320)
 
-btn_find_edges = Button(my_w, text="Edges", bg='#8758FF',fg='black',
+btn_find_edges = Button(my_w, text="Edges", bg='#8758FF', fg='black',
                     font='ariel 12 bold', relief=GROOVE, command=find_edges, width=10)
 btn_find_edges.place(x=180, y=320)
 
-
+# section Layout
 rotate = Label(my_w, text="Rotate:", font="ariel 12 bold")
 rotate.place(x=40, y=400)
 values = [0, 90, 180, 270, 360]
 rotate_combo = ttk.Combobox(my_w, values=values, font='ariel 10 bold')
 rotate_combo.place(x=100, y=400)
 rotate_combo.bind("<<ComboboxSelected>>", rotate_image)
-
 
 flip = Label(my_w, text="Flip:", font="ariel 12 bold")
 flip.place(x=40, y=430)
@@ -305,6 +307,7 @@ flip_combo = ttk.Combobox(my_w, values=opt_flip, font='ariel 10 bold')
 flip_combo.place(x=100, y=430)
 flip_combo.bind("<<ComboboxSelected>>", flip_img)
 
+# section Pruning tools
 btn_crop = Button(my_w, text="Crop", bg='#D2001A', fg='black',
                     font='ariel 12 bold', relief=GROOVE, command=crop_img, width=10)
 btn_crop.place(x=40, y=505)
@@ -329,21 +332,7 @@ crop_bot_label.place(x=245, y=520)
 crop_bot_entry = Entry(width=8)
 crop_bot_entry.place(x=275, y=520)
 
-solarize_label = Label(my_w, text="Solarize:", font=("ariel 13 bold"), width=9, anchor='e')
-solarize_label.place(x=15, y=170)
-v1 = IntVar()
-scale1 = ttk.Scale(my_w, from_=0, to=255, variable=v1,
-                   orient=HORIZONTAL, command=solarize)
-scale1.place(x=130, y=170)
-
-postersize_label = Label(my_w, text="Postersize:", font=("ariel 13 bold"), width=9, anchor='e')
-postersize_label.place(x=15, y=200)
-v2 = IntVar()
-scale2 = ttk.Scale(my_w, from_=1, to=5, variable=v2,
-                   orient=HORIZONTAL, command=postersize)
-scale2.place(x=130, y=200)
-
-
+# section Watermark
 btn_water = Button(my_w, text="Add", bg='#FD841F', fg='black',
                     font='ariel 12 bold', relief=GROOVE, command=watermark, width=10)
 btn_water.place(x=40, y=580)
